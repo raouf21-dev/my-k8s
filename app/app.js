ef9bc4a60aed72ea6051faff1b60b1b30f9d57da
Raouf <raouf_dev@hotmail.com>
2025-05-29 16:38:58 +0100

    added dockerfile

---
+++ app/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware tp parse JSON req
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.json({message: "Hello K8s"})
+})
+
+// Start Server
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`);
+})
